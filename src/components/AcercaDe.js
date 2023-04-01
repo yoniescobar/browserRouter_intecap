0e32cecabfb6c3d51bb7929dcf6d1ca24fcf16cd
yoniedilzar <yoniedilzar@gmail.com>
2023-04-01 16:50:58 -0600

    autor

--- src/components/AcercaDe.js
+++ src/components/AcercaDe.js
@@ -5,6 +5,7 @@ const AcercaDe = () => {
         <div>
             <h3>Pagina Acerca de...</h3>
             <p>Esta es la página de acercad de Intecap</p>
+            <h4>Desarrollador: Yoni Escobar</h4>
         </div>
     )
 }
