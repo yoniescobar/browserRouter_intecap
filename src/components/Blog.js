7b27575b71f937851bdb77dacd727adcc039da40
yoniedilzar <yoniedilzar@gmail.com>
2023-04-01 15:39:18 -0600

    menejo post

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import posts from './../data/posts'
+import { NavLink } from 'react-router-dom'
 
 const Blog = () => {
+    console.log(posts)
     return (
         <div>
             <h3>Página de Blog</h3>
             <p>Esta es la página de Blog</p>
             <ul>
-                <li>Post 1</li>
-                <li>Post 2</li>
-                <li>Post 3</li>
+                {
+                    posts.map((post)=>(
+                        <li key={post.id}>
+                            <NavLink to={`/blog/${post.id}`}>{post.title}</NavLink>
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     )
---
+++ src/data/posts.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {id: 1, title: 'JavaScript', content: 'JavaScript es un lenguaje de programación interpretado, dialecto del estándar ECMAScript. Se define como orientado a objetos,​ basado en prototipos, imperativo, débilmente tipado y dinámico.'},
+    {id: 2, title: 'Python', content: 'Python es un lenguaje de programación interpretado cuya filosofía hace hincapié en una sintaxis que favorezca un código legible.​'},
+    {id: 3, title: 'Java', content: 'Java es un lenguaje de programación y una plataforma informática comercializada por primera vez en 1995 por Sun Microsystems. Hay muchas aplicaciones y sitios web que no funcionarán, a menos que tenga Java instalado y cada día se crean más. Java es rápido, seguro y fiable.'},
+    {id: 4, title: 'C++', content: 'C++ es un lenguaje de programación de propósito general, compilado, multi-paradigma y multiplataforma.​ Fue diseñado por Bjarne Stroustrup comenzando en 1979 en Bell Labs como un lenguaje de mejoras para el lenguaje de programación C, llamado inicialmente C con clases.'},
+    {id: 5, title: 'C#', content: 'C# es un lenguaje de programación multiparadigma desarrollado por Microsoft como parte de su plataforma .NET. Fue anunciado en 2000 y posteriormente lanzado en 2002.​'},
+    {id: 6, title: 'PHP', content: 'PHP: Hypertext Preprocessor es un lenguaje de programación de uso general de código del lado del servidor originalmente diseñado para el desarrollo web de contenido dinámico. Fue uno de los primeros lenguajes de programación del lado del servidor que se podían incrustar directamente en el HTML.'},
+    {id: 7, title: 'Ruby', content: 'Ruby es un lenguaje de programación interpretado, multiparadigma y de tipado dinámico. Fue diseñado y desarrollado en los años 1990 por Yukihiro Matsumoto en Japón.​'},
+    {id: 8, title: 'Swift', content: 'Swift es un lenguaje de programación desarrollado por Apple Inc. para programar las aplicaciones para sus sistemas operativos iOS, iPadOS, macOS, watchOS y tvOS.​'},
+    {id: 9, title: 'Go', content: 'Go, también conocido como Golang, es un lenguaje de programación concurrente y compilado desarrollado por Google.​'},
+    {id: 10, title: 'Kotlin', content: 'Kotlin es un lenguaje de programación multiplataforma, orientado a objetos y funcional, desarrollado por JetBrains.​'},
+    {id: 11, title: 'Rust', content: 'Rust es un lenguaje de programación multiparadigma desarrollado por Mozilla Research.​'},
+    {id: 12, title: 'TypeScript', content: 'TypeScript es un lenguaje de programación libre y de código abierto desarrollado y mantenido por Microsoft. Es un superset de JavaScript, y añade tipado estático y objetos basados en clases.'},
+    {id: 13, title: 'C', content: 'C es un lenguaje de programación de propósito general, imperativo, estructurado, compilado, que se diseñó para desarrollar el sistema operativo UNIX.​'},
+    {id: 14, title: 'Cobol', content: 'COBOL es un lenguaje de programación de propósito general, imperativo, estructurado, compilado, que se diseñó para desarrollar el sistema operativo UNIX.​'},
+    {id: 15, title: 'Dart', content: 'Dart es un lenguaje de programación desarrollado por Google.​'},
+    {id: 16, title: 'Elixir', content: 'Elixir es un lenguaje de programación funcional, concurrente y de propósito general, basado en la máquina virtual Erlang.​'},
+    {id: 17, title: 'Erlang', content: 'Erlang es un lenguaje de programación concurrente, funcional y de propósito general, desarrollado por Ericsson.​'},
+    {id: 18, title: 'F#', content: 'F# es un lenguaje de programación funcional, multi-paradigma y multiplataforma desarrollado por Microsoft.​'},
+    {id: 19, title: 'Groovy', content: 'Groovy es un lenguaje de programación multiplataforma, orientado a objetos y de tipado dinámico, que se ejecuta en la Máquina Virtual de Java (JVM).​'},
+    {id: 20, title: 'Haskell', content: 'Haskell es un lenguaje de programación puramente funcional, estaticamente tipado y con tipado de inferencia.​'},
+
+
+];
